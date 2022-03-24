ec48d1e976582684bdc103cc520446a146d80593
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-24 10:26:34 +0100

    Ejercicio 9 terminado (Clases abstractas)

---
+++ u6Domingo/src/main/java/Ejercicios/ejercicio9/ClasesAbstractas.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios.ejercicio9;
+
+/**
+ *
+ * @author domingo
+ */
+public class ClasesAbstractas {
+    
+    /* ¿Puede tener descendencia esta clase? */
+    // Sí y debe de tenerla porque es un molde
+    
+    /* ¿Se pueden sobrescribir todos sus métodos? */
+    // No se pueden sobreescribir todos los metodos ya que existen
+    // metodos constantes (final) los cuales dará error al crear
+    // otro metodo
+    
+    /* ¿Qué pasa si en vez de public abstract vehiculo ponemos public final class vehiculo */
+    // Si es final no debe tener descendencia
+}
