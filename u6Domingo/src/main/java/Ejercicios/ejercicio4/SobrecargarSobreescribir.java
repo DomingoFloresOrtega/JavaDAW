5916af167bbec2b3105ea95ba19bbea065c2d37b
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-24 10:27:15 +0100

    Ejercicio 4 terminado (Sobrecarga)

---
+++ u6Domingo/src/main/java/Ejercicios/ejercicio4/SobrecargarSobreescribir.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios.ejercicio4;
+
+/**
+ *
+ * @author domingo
+ */
+public class SobrecargarSobreescribir {
+    // Al sobrecargar un metodo estamos usando los mismos nombres para los metodos
+    // pero se diferencia la funcionalidad del metodo
+    // mientras que al sobreescribir un metodo anulamos al metodo anterior
+    // y sustituimos la funcionalidad por la del metodo actual
+}
