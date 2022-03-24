b7af070c9728ccbcda14f54efc0f3c88cfd80fae
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-24 10:48:39 +0100

    Teoria sobre enum

---
+++ u5Domingo/src/main/java/teoria/TeoriaEnum.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teoria;
+
+import java.time.Month;
+
+/**
+ *
+ * @author domingo
+ */
+public class TeoriaEnum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /* Teoria (02/02/2022) */
+
+        Month marzo = Month.MARCH;
+
+        // Para crear una clase enum, pulsamos en New > Java Class > Other > Java Enum
+        // La clase sera de tipo:
+        // public enum nombreClase
+    }
+}
