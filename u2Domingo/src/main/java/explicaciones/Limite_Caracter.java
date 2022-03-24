5f91afae1a1b4e4ffd4d973e8498147cf04d4ea9
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-24 11:15:14 +0100

    Teoría sobre el limite de un caracter

---
+++ u2Domingo/src/main/java/explicaciones/Limite_Caracter.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package explicaciones;
+
+/**
+ *
+ * @author domingo
+ */
+public class Limite_Caracter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // printf("%._f",a)
+        // _ --> numero de caracteres / a --> variable a mostrar
+        
+        double a = 7.370;
+        double b = 8.41245;
+       
+        System.out.printf("%.2f %.3f", a,b);
+    }
+    
+}
