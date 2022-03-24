b245e663f5f6157c9b3eb1a1c735e47e9efa7f60
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-24 10:46:35 +0100

    Tarea 5D terminado (Ejercito)

---
+++ u5Domingo/src/main/java/tarea5D/Ejercito.java
@@ -0,0 +1,62 @@
+package tarea5D;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ejercito {
+    public static void main(String[] args) {
+        Set<Soldado> ejercito = new HashSet<>();
+
+        ejercito.add(new Soldado("34904589J","Antonio","Martinez","Muñoz",20));
+        ejercito.add(new Soldado("23096489O","Pedro","Benitez","Muñoz",25));
+        ejercito.add(new Soldado("12987048I","Mario","Martinez","Estrada",32));
+        ejercito.add(new Soldado("90483920L","Adolfo","Perez","García",30));
+
+        saberNumeroSoldados(ejercito);
+        alistarSoldado(ejercito);
+        estaVacio(ejercito);
+        estaEjercito(ejercito);
+        mostrarEjercitoArray(ejercito);
+        desmatricularSoldado(ejercito);
+    }
+
+    public static void saberNumeroSoldados(Set<Soldado> ejercito){
+        for (Soldado p : ejercito) {
+            System.out.println(p);
+        }
+    }
+
+    public static void alistarSoldado(Set<Soldado> ejercito){
+        ejercito.add(new Soldado("89340987U", "Pepe", "Martinez","García",34));
+    }
+
+    public static boolean estaVacio(Set<Soldado> ejercito){
+        return ejercito.isEmpty();
+    }
+    
+    public static boolean estaEjercito(Set<Soldado> ejercito){
+        return ejercito.contains(new Soldado("89340987U","Pepe","Martinez","García",34));
+    }
+    
+    public static ArrayList mostrarEjercitoArray(Set<Soldado> ejercito){
+        ArrayList<Soldado> arraySoldados= new ArrayList<>();
+        
+        for (Soldado p : ejercito) {
+            arraySoldados.add(p);
+        }
+        
+        // El objeto iterator sirve para recorrer todos los elementos de una estructura de datos
+        // Es equivalente a usar el for each
+//         Iterator<Soldado> iteral - listaSoldados.iterator();
+//         while(iteral.hasNext()){
+//            Soldado au = iteral.next();
+//         }
+        
+        return arraySoldados;
+    }
+
+    public static void desmatricularSoldado(Set<Soldado> ejercito){
+        ejercito.remove(new Soldado("90483920L","Adolfo","Perez","García",30));
+    }
+}
---
+++ u5Domingo/src/main/java/tarea5D/Soldado.java
@@ -0,0 +1,93 @@
+package tarea5D;
+
+import java.util.Objects;
+
+public class Soldado {
+    private String nif;
+    private String nombre;
+    private String ape1;
+    private String ape2;
+    private int edad;
+
+    public Soldado(String nif, String nombre, String ape1, String ape2, int edad) {
+        this.nif = nif;
+        this.nombre = nombre;
+        this.ape1 = ape1;
+        this.ape2 = ape2;
+        this.edad = edad;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApe1() {
+        return ape1;
+    }
+
+    public void setApe1(String ape1) {
+        this.ape1 = ape1;
+    }
+
+    public String getApe2() {
+        return ape2;
+    }
+
+    public void setApe2(String ape2) {
+        this.ape2 = ape2;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Soldado{" +
+                "nif='" + nif + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", ape1='" + ape1 + '\'' +
+                ", ape2='" + ape2 + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Soldado other = (Soldado) obj;
+        
+        return Objects.equals(this.nif, other.nif);
+    }
+
+    
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nif, nombre, ape1, ape2, edad);
+    }
+}
