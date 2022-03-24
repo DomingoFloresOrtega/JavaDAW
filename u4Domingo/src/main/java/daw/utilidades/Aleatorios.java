b4bde58e6ed2e08692d0d233b8b37eaa03fc9d37
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-24 12:38:45 +0100

    Clase utilidades (Aleatorios)

---
+++ u4Domingo/src/main/java/daw/utilidades/Aleatorios.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw.utilidades;
+
+/**
+ *
+ * @author domingo
+ */
+public class Aleatorios {
+    public void enteroAleatorio(){
+          
+    }
+    
+    public int enteroRango(int min, int max){
+        int num = 
+        return num;
+    }
+    
+    public void letraMinuscula(){
+        
+    }
+    
+    public void letraMayuscula(){
+        
+    }
+}
