480024296861486bf55799d9d3c0454ee8ad2d57
Domingo <domingo@iesnuevaandalucia.com>
2022-03-29 16:40:46 +0200

    Ejercicio 14 en casa (Electrodomesticos)

--- u6Domingo/src/main/java/Ejercicios/ejercicio14/CampanaExtractora.java
+++ u6Domingo/src/main/java/Ejercicios/ejercicio14/CampanaExtractora.java
@@ -8,6 +8,32 @@ package Ejercicios.ejercicio14;
  *
  * @author domingo
  */
-public class CampanaExtractora {
-    
+public class CampanaExtractora extends Electrodomestico {
+    private double decibelios;
+
+    public CampanaExtractora(double consumo, String modelo) {
+        super(consumo, modelo);
+    }
+
+    public CampanaExtractora(double consumo, String modelo, double decibelios) {
+        super(consumo, modelo);
+        if (decibelios <= 50) {
+            this.decibelios = decibelios;
+        }
+    }
+
+    public double getDecibelios() {
+        return decibelios;
+    }
+
+    public void setDecibelios(double decibelios) {
+        this.decibelios = decibelios;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "CampanaExtractora{" +
+                "decibelios=" + decibelios +
+                '}';
+    }
 }
