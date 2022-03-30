fc8a7020a01b9d3491dde120f5ef748c51ccf98f
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-30 13:48:42 +0200

    Ejercicio 14 / 18 terminado (Electrodomesticos)

--- u6Domingo/src/main/java/Ejercicios/ejercicio14/Test.java
+++ u6Domingo/src/main/java/Ejercicios/ejercicio14/Test.java
@@ -6,12 +6,15 @@ public class Test {
     public static void main(String[] args) {
         ArrayList<Silencioso> lista = new ArrayList<>();
 
-        CampanaExtractora c1 = new Electrodomestico();
-        Frigorifico f1 = new Electrodomestico();
-        Microondas m1 = new Electrodomestico();
+        CampanaExtractora c1 = new CampanaExtractora(20,"Siemens");
+        Frigorifico f1 = new Frigorifico(40,"Samsung");
+        Microondas m1 = new Microondas(30,"LG");
 
         lista.add(c1); // No se puede agregar a la lista ya que el objeto CampanaExtractora no es un objeto de tipo Silencioso
         lista.add(f1);
         lista.add(m1);
+        
+        // Comparable -> Orden natural
+        // Comparator -> 
     }
 }
