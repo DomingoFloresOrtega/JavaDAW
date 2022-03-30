11e6b3e55c51b2a07f30c23b99cbe37232d62eb0
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-30 13:55:31 +0200

    Teoría sobre compareTo (30/03/2022)

---
+++ u6Domingo/src/main/java/Teoria/compareTo/Teoria.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Teoria.compareTo;
+
+/**
+ *
+ * @author domingo
+ */
+public class Teoria {
+    // El metodo compareTo devuelve
+    // -1 -> +pequeño
+    // +1 -> grande
+    // 0 -> igual
+    // Con el metodo sort podemos ordenar los objetos
+}
