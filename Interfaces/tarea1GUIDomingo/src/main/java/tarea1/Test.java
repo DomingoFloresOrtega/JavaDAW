62691a6584f8b9cbe91ae01498e78d18ddc6473e
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-31 11:08:56 +0200

    Panel básico

---
+++ Interfaces/tarea1GUIDomingo/src/main/java/tarea1/Test.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package tarea1;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author domingo
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        // Construimos la ventana
+        JFrame ventanaPrincipal = new JFrame("Aplicación");
+
+        // Establecemos tamaño y posición
+        ventanaPrincipal.setSize(800, 600);
+        ventanaPrincipal.setLocationRelativeTo(null);
+
+        // Añadimos un objeto MiPanel al JFrame
+        ventanaPrincipal.add(new MiPanel(Color.cyan));
+
+        // Hacemos visible la ventana
+        ventanaPrincipal.setVisible(true);
+
+        // Acción por defecto al pulsar el botón de cierre de la ventana
+        ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
