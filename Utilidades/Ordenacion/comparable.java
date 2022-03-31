262ae3b3eb410ba5edc0db39641467837c8c1294
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-31 12:23:15 +0200

    Ordenar (Comparable)

---
+++ Utilidades/Ordenacion/comparable.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Numeros.aleatorios;
+
+/**
+ *
+ * @author domingo
+ */
+public class numerosAleatorios {
+    // Constructor de aleatorio
+    aleatorio = new Random();
+    
+    // Generar aleatorios entre dos numeros
+    aleatorio.nextInt(MAX-MIN+1)-MIN;
+    
+    // Generar 100 aleatorios (de 0 a 99)
+    aleatorio.nextInt(100);
+    
+    // Generar 100 aleatorios (de 1 a 100)
+    aleatorio.nextInt(100-1)
+}
