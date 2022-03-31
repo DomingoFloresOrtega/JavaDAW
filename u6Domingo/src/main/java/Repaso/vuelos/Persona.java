bfe67ad7924fa22e52883d9317a6674af32733c5
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-31 10:48:31 +0200

    Tarea de repaso en clase (Vuelos)

---
+++ u6Domingo/src/main/java/Repaso/vuelos/Persona.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Repaso.vuelos;
+
+/**
+ *
+ * @author domingo
+ */
+public class Pasajero {
+    private String nombre;
+    private String apellidos;
+    private String nif;
+
+    public Pasajero() {
+    }
+
+    public Pasajero(String nombre, String apellidos, String nif) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    @Override
+    public String toString() {
+        return "Pasajero{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", nif=" + nif + '}';
+    }
+}
