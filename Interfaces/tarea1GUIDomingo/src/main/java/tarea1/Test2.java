8cf96ead69f658096fdf625343f94aae72a6482b
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-31 11:09:04 +0200

    Panel avanzado

---
+++ Interfaces/tarea1GUIDomingo/src/main/java/tarea1/Test2.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package tarea1;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author domingo
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        // Construimos la ventana
+        JFrame ventanaPrincipal = new JFrame("Aplicación");
+
+        // Establecemos posición y tamaño
+        ventanaPrincipal.setBounds(250, 250, 800, 600);
+
+        // Hacemos visible la ventana
+        ventanaPrincipal.setVisible(true);
+
+        // Acción por defecto al pulsar el botón de cierre de la ventana
+        ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Establecemos posición y tamaño
+        // ventanaPrincipal.setBounds(250, 250, 800, 600);
+        ventanaPrincipal.setSize(800, 600);
+        ventanaPrincipal.setLocationRelativeTo(null);
+
+        // Establecemos el layout del JFrame
+        ventanaPrincipal.setLayout(new BorderLayout());
+
+        // Añadimos los paneles en cada posición del BorderLayout
+        // Añadir panel amarillo en la posición superior (NORTH)
+        ventanaPrincipal.add(new MiPanel(Color.yellow), BorderLayout.NORTH);
+
+        // Añadir panel rojo en la posición inferior (SOUTH)
+        ventanaPrincipal.add(new MiPanel(Color.red), BorderLayout.SOUTH);
+        
+        // Añadir panel gris en la posición central (CENTER)
+        ventanaPrincipal.add(new MiPanel(Color.gray), BorderLayout.CENTER);
+
+        // Añadir panel verde en la posición izquierda (WEST)
+        ventanaPrincipal.add(new MiPanel(Color.green), BorderLayout.WEST);
+
+        // Añadir panel cián en la posición derecha (EAST)
+        ventanaPrincipal.add(new MiPanel(Color.cyan), BorderLayout.EAST);
+
+    }
+}
