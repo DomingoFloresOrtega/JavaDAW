109675375ef0123f6f94c26db05b61c96328bb01
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-03-31 10:45:51 +0200

    Tarea de repaso en clase (Vuelos)

---
+++ u6Domingo/src/main/java/Repaso/vuelos/Vuelo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Repaso.vuelos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author domingo
+ */
+public class Vuelo {
+    private int codigo;
+    private String ciudad;
+    private String ciudadDestino;
+    private double duracion;
+    private ArrayList<Pasajero> pasajeros;
+}
