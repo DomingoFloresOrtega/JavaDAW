d0af6482679faa0cc41b948e55e5eb06f53ab70e
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-04-19 14:24:56 +0200

    Ejercicio 2 terminado

--- u7Domingo/src/main/java/Ejercicios/ejercicio2/Main.java
+++ u7Domingo/src/main/java/Ejercicios/ejercicio2/Main.java
@@ -29,7 +29,7 @@ public class Main {
                 System.out.println("Introduzca texto:");
                 String texto = teclado.nextLine();
 
-                if (texto.equals("EOF")){
+                if (texto.equalsIgnoreCase("EOF")){
                     terminar = false;
                 } else {
                     flujo.write(texto);
