fcccb04bc2209870c78d2195993984009c8fb789
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-04-19 14:42:10 +0200

    Ejercicio 4 en clase

---
+++ u7Domingo/src/main/java/Ejercicios/ejercicio4/Main.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejercicios.ejercicio4;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author domingo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList lista = new ArrayList<>();
+        
+        // Creo turismos
+        lista.add(new Turismo(312333L, "9837UIK", "Ford", "Fiesta", "Rojo", 2, 3, true));
+        lista.add(new Turismo(312333L, "9837UIK", "Ford", "Fiesta", "Rojo", 2, 3, true));
+        lista.add(new Turismo(312333L, "9837UIK", "Ford", "Fiesta", "Rojo", 2, 3, true));
+        lista.add(new Turismo(312333L, "9837UIK", "Ford", "Fiesta", "Rojo", 2, 3, true));
+        lista.add(new Turismo(312333L, "9837UIK", "Ford", "Fiesta", "Rojo", 2, 3, true));
+        lista.add(new Turismo(312333L, "9837UIK", "Ford", "Fiesta", "Rojo", 2, 3, true));
+        lista.add(new Turismo(312333L, "9837UIK", "Ford", "Fiesta", "Rojo", 2, 3, true));
+        lista.add(new Turismo(312333L, "9837UIK", "Ford", "Fiesta", "Rojo", 2, 3, true));
+        lista.add(new Turismo(312333L, "9837UIK", "Ford", "Fiesta", "Rojo", 2, 3, true));
+        lista.add(new Turismo(312333L, "9837UIK", "Ford", "Fiesta", "Rojo", 2, 3, true));
+        
+        // Creo deportivos
+        
+    }
+    
+}
