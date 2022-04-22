75cb01b4535989f091fea1aa9bf7ca8d7a87334d
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-04-22 13:56:51 +0200

    Tarea 7a en clase

--- u7Domingo/src/main/java/Tareas/tarea7a/Main.java
+++ u7Domingo/src/main/java/Tareas/tarea7a/Main.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  *
@@ -34,9 +35,9 @@ public class Main {
         String idFichero = "RelPerCen.csv";
         String[] tokens;
         String linea;
-        Map<String,Integer> lista = new HashMap();
+        ArrayList<Pojo> lista = new ArrayList<>();
 
-        try ( Scanner datosFichero = new Scanner(new File(idFichero), "ISO-8859-1")) {
+        try (Scanner datosFichero = new Scanner(new File(idFichero), "ISO-8859-1")) {
             datosFichero.nextLine();
             while (datosFichero.hasNextLine()) {
                 linea = datosFichero.nextLine();
@@ -48,7 +49,7 @@ public class Main {
                 p1.setPuesto(noComillas(tokens[3]));
                 p1.setFechaToma(LocalDate.parse(noComillas(tokens[4]), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
                 
-                if (tokens[5].length() <= 0) {
+                if (noComillas(tokens[5]).length() <= 0) {
                     p1.setFechaCese(LocalDate.now());
                 } else {
                     p1.setFechaCese(LocalDate.parse(noComillas(tokens[5]), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
@@ -67,25 +68,46 @@ public class Main {
                 } else {
                     p1.setCoordinador(false);
                 }
+                
+                lista.add(p1);
             }
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
 
         // Muestro lista
+        System.out.println(lista.size());
         for (int i = 0; i < lista.size(); i++){
             System.out.println(lista.get(i));
         }
 
+        // Convierto lista a map
+        Map<String, Integer> listaProfesores = new HashMap();
+        
+        int contadorDepartamento = 1;
+        for (Pojo profesor : lista) {
+            if (listaProfesores.containsKey(profesor.getPuesto())) {
+                contadorDepartamento++;
+                listaProfesores.put(profesor.getPuesto(), contadorDepartamento);
+            } else {
+                contadorDepartamento = 1;
+                listaProfesores.put(profesor.getPuesto(), contadorDepartamento);
+
+            }
+        }
+        
+        Map<String, Integer> listaMap = new TreeMap();
+        listaMap = listaProfesores;
+        
         // Muestro lista por departamento y guardo en archivo
         String idFichero2 = "profesoresPorDepartamento.txt";
 
         try ( BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero2))) {
             flujo.write("Departamentos\tNúmero");
-            //for (String key : lista) {
-            //    flujo.write(key + " \t " + lista.get(key));
-            //    flujo.newLine();
-            //}
+            for (String key : listaMap.keySet()) {
+                flujo.write(key + " \t " + listaMap.get(key));
+                flujo.newLine();
+            }
 
             flujo.flush();
             System.out.println("Fichero " + idFichero2 + " generado correctamente.");
