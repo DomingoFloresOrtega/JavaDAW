25081da5b07ddff1a525d9d8f27e87c83027470c
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-04-27 14:42:47 +0200

    Ejercicio 7 en clase

--- u7Domingo/src/main/java/Ejercicios/ejercicio7/Main.java
+++ u7Domingo/src/main/java/Ejercicios/ejercicio7/Main.java
@@ -4,6 +4,9 @@
  */
 package Ejercicios.ejercicio7;
 
+import Ejercicios.ejercicio4.Deportivo;
+import Ejercicios.ejercicio4.Furgoneta;
+import Ejercicios.ejercicio4.Turismo;
 import Ejercicios.ejercicio4.Vehiculo;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -29,11 +32,48 @@ public class Main {
             while (datosFichero.hasNextLine()) {
                 linea = datosFichero.nextLine();
 
-                tokens = linea.split("\t");
-                for (String string : tokens) {
-                    for (int i = 0; i < tokens.length; i++){
-                        
-                    }
+                tokens = linea.split(";");
+                
+                switch (Integer.parseInt(tokens[0])) {
+                    case 0:
+                        Turismo t1 = new Turismo();
+                        t1.setBastidor(Long.parseLong(tokens[1]));
+                        t1.setMatricula(tokens[2]);
+                        t1.setMarca(tokens[3]);
+                        t1.setModelo(tokens[4]);
+                        t1.setColor(tokens[5]);
+                        t1.setTarifa(Double.parseDouble(tokens[6]));
+                        t1.setDisponible(Boolean.parseBoolean(tokens[7]));
+                        t1.setPuertas(Integer.parseInt(tokens[8]));
+                        t1.setMarchaAutomatica(Boolean.parseBoolean(tokens[9]));
+                        array.add(t1);
+                        break;
+                    case 1:
+                        Deportivo d1 = new Deportivo();
+                        d1.setBastidor(Long.parseLong(tokens[1]));
+                        d1.setMatricula(tokens[2]);
+                        d1.setMarca(tokens[3]);
+                        d1.setModelo(tokens[4]);
+                        d1.setColor(tokens[5]);
+                        d1.setTarifa(Double.parseDouble(tokens[6]));
+                        d1.setDisponible(Boolean.valueOf(tokens[7]));
+                        d1.setCilindrada(Integer.parseInt(tokens[8].substring(0, tokens[8].length() - 1)));
+
+                        array.add(d1);
+                        break;
+                    case 2:
+                        Furgoneta f1 = new Furgoneta();
+                        f1.setBastidor(Long.parseLong(tokens[1]));
+                        f1.setMatricula(tokens[2]);
+                        f1.setMarca(tokens[3]);
+                        f1.setModelo(tokens[4]);
+                        f1.setColor(tokens[5]);
+                        f1.setTarifa(Double.parseDouble(tokens[6]));
+                        f1.setDisponible(Boolean.valueOf(tokens[7]));
+                        f1.setCarga(Integer.parseInt(tokens[8]));
+                        f1.setVolumen(Integer.parseInt(tokens[9].substring(0, tokens[9].length() - 1)));
+                        array.add(f1);
+                        break;
                 }
             }
         } catch (FileNotFoundException e) {
