3b2bad44ea3ba10f3b74cfe6e64b525960481f34
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-04-29 13:44:11 +0200

    Ejercicio 10 en clase

--- u7Domingo/src/main/java/Ejercicios/ejercicio10/Main.java
+++ u7Domingo/src/main/java/Ejercicios/ejercicio10/Main.java
@@ -26,49 +26,30 @@ public class Main {
     public static void main(String[] args) {
         String ruta = "./copias";
         String rutaOriginal = "/";
-        String rutaOrigen = ".";
-        String rutaDestino = ".";
-        String ficheroT = "./copias/turismos.txt";
-        String ficheroD = "./copias/deportivos.txt";
-        String ficheroF = "./copias/furgonetas.txt";
-        String[] tokens;
-        String linea;
-        ArrayList<Vehiculo> array = new ArrayList<>();
-
 
         // Crear directorio
         Path directory = Paths.get(ruta);
 
         try {
             Files.createDirectory(directory);
-
         } catch (IOException ioe) {
             System.out.println("Problema creando el directorio " + ruta);
-            System.out.println("Seguramente la ruta está mal escrita o no existe");
-
-        } catch (FileAlreadyExistsException faee) {
+        //} catch (FileAlreadyExistsException faee) {
             System.out.println("No se puede crear " + ruta + " porque ya existe");
-
-        } catch (AccessDeniedException ade) {
+        //} catch (AccessDeniedException ade) {
             System.out.println("No tiene permisos para crear " + ruta);
         }
 
         // Copiar archivos
-        Path origen = Paths.get(rutaOrigen);
-        Path destino = Paths.get(rutaDestino);
-        try {
-            Files.copy(origen, destino);
-        } catch (IOException e) {
-            System.out.println("Existe un problema al copiar el archivo");
-            System.out.println(e.toString());
-        }
-
+        copiarFichero("./turismos.txt","./copias");
+        copiarFichero("./deportivos.txt","./copias");
+        copiarFichero("./furgonetas.txt","./copias");
 
         // Listar directorio
-        File file = new File(ruta);
+        File fileD = new File(ruta);
 
-        if (file.exists()) {
-            File[] ficheros = file.listFiles();
+        if (fileD.exists()) {
+            File[] ficheros = fileD.listFiles();
             for (File file2 : ficheros) {
                 System.out.println(file2.getName());
             }
@@ -77,21 +58,10 @@ public class Main {
         }
 
         // Leer ficheros
-        try (Scanner datosFichero = new Scanner(new File(ficheroT), "UTF-8")) {
-            while (datosFichero.hasNextLine()) {
-                linea = datosFichero.nextLine();
-                tokens = linea.split(";");
-
-                for (String v : tokens) {
-                    array.add(v);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
+        ArrayList<Vehiculo> array = leerFichero(ruta);
 
         // Ordenar la lista
-        array.sort((a1,a2)->a1.getBastidor().compareTo(a2.getBastidor()));
+        array.sort((a1, a2) -> a1.getBastidor().compareTo(a2.getBastidor()));
 
         // Imprimir lista
         for (Vehiculo v : array) {
@@ -99,6 +69,34 @@ public class Main {
         }
 
         // Borrar ficheros
+        borrarFichero(ruta);
+        
+        // Listar directorio original
+        listarDirectorioOriginal(rutaOriginal);
+        
+        /********************
+        ****** STREAMS ******
+        ********************/
+        
+        // Imprime por pantalla todos los coches blancos, distintos, ordenador por matrícula.
+        array.stream().filter(coche -> coche.getColor().equalsIgnoreCase("Blanco"))
+                .distinct()
+                .sorted((c1,c2)->c1.getMatricula().compareTo(c2.getMatricula()));
+        array.forEach(System.out::println);
+    }
+
+    public static void copiarFichero(String rutaOrigen, String rutaDestino) {
+        Path origen = Paths.get(rutaOrigen);
+        Path destino = Paths.get(rutaDestino);
+        try {
+            Files.copy(origen, destino);
+        } catch (IOException e) {
+            System.out.println("Existe un problema al copiar el archivo");
+            System.out.println(e.toString());
+        }
+    }
+
+    public static void borrarFichero(String ruta) {
         Path element = Paths.get(ruta);
 
         try {
@@ -110,8 +108,30 @@ public class Main {
         } catch (IOException e) {
             System.out.println("Problema borrando el elemento " + ruta);
         }
+    }
+    
+    public static ArrayList<Vehiculo> leerFichero(String fichero){
+        String[] tokens;
+        String linea;
+        ArrayList<Vehiculo> array = new ArrayList<>();
+        
+        try ( Scanner datosFichero = new Scanner(new File(fichero), "UTF-8")) {
+            while (datosFichero.hasNextLine()) {
+                linea = datosFichero.nextLine();
+                tokens = linea.split(";");
 
-        // Listar directorio original
+                for (String v : tokens) {
+                    array.add(v);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return array;
+    }
+    
+    public static void listarDirectorioOriginal(String rutaOriginal){
         File file = new File(rutaOriginal);
 
         if (file.exists()) {
@@ -122,4 +142,5 @@ public class Main {
         } else {
             System.out.println("El directorio a listar no existe");
         }
+    }
 }
