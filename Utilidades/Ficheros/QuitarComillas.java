bf0530b19088c182029b43286dbfb00d9c312d98
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-05-10 13:44:45 +0200

    Quitar las comillas

---
+++ Utilidades/Ficheros/QuitarComillas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author domingo
+ */
+public class QuitarComillas {
+    private static String comilla(String s) {
+        String quitarComilla = s.substring(1, s.length() - 1);
+        return quitarComilla;
+    }
+}
