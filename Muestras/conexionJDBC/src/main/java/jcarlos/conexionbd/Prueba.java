5a4f1f64de5297d7dac4effec31de47ec1619c8b
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-05-11 13:52:53 +0200

    Ejercicios de muestra para conectar con BBDD

---
+++ Muestras/conexionJDBC/src/main/java/jcarlos/conexionbd/Prueba.java
@@ -0,0 +1,14 @@
+
+package jcarlos.conexionbd;
+
+import java.sql.Connection;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        
+        Connection con = Conexion.getInstance();
+        
+    }
+    
+}
