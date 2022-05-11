594b9437cf796c4a0579c99bcc80d15a567539d9
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-05-11 14:03:41 +0200

    Practica de repaso unidades 1-7 en clase

---
+++ Examenes/Simulacros/repasoU17Domingo/src/main/java/repaso/LecturaCSV.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repaso;
+
+/**
+ *
+ * @author domingo
+ */
+public class LecturaCSV {
+    public static void leerCSV(String nombreFichero, String rutaFichero){
+        
+    }
+}
