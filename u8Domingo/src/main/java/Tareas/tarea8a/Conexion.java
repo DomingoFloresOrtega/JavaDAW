5be90d5076f1a805dac764e6f208d415eea3c4ce
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-05-13 09:25:35 +0200

    Tarea 8a en clase terminada

--- u8Domingo/src/main/java/Tareas/tarea8a/Conexion.java
+++ u8Domingo/src/main/java/Tareas/tarea8a/Conexion.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-
-    private static final String SERVIDOR = "jdbc:mysql://localhost/";
+    
+    private static final String SERVIDOR = "jdbc:mysql://192.168.56.3:3306/";
     private static final String NOMBRE_BASE_DATOS = "prog2122";
     private static final String USER = "domingo";
     private static final String PASS = "d0m1ng0@";
@@ -25,7 +25,6 @@ public class Conexion {
         // Si el objeto Connection no está creado, se crea
         if (instancia == null) {
             try {
-
                 // Se crea el objeto Connection	
                 instancia = DriverManager.getConnection(SERVIDOR + NOMBRE_BASE_DATOS, USER, PASS);
 
@@ -39,5 +38,4 @@ public class Conexion {
         }
         return instancia;
     }
-
 }
