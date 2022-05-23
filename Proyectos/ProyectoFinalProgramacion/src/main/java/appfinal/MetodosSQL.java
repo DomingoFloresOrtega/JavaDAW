4137379bfb307648707da4300de83018f82547bb
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-05-23 18:23:55 +0200

    Agregado el primer metodo para sentecia SQL

--- Proyectos/ProyectoFinalProgramacion/src/main/java/appfinal/MetodosSQL.java
+++ Proyectos/ProyectoFinalProgramacion/src/main/java/appfinal/MetodosSQL.java
@@ -17,5 +17,12 @@ public class MetodosSQL {
     public static void matricularAlumno(){
         Alumnado a1 = new Alumnado();
         a1.setNombreAlumnado(String.valueOf(JOptionPane.showInputDialog("Inserte el nombre del alumno")));
+        a1.setApe1Alumnado(String.valueOf(JOptionPane.showInputDialog("Inserte el primer apellido del alumno")));
+        a1.setApe2Alumnado(String.valueOf(JOptionPane.showInputDialog("Inserte el segundo apellido del alumno (si tiene)")));
+        a1.setDireccion(String.valueOf(JOptionPane.showInputDialog("Inserte la direcció del alumno")));
+        a1.setProvincia(String.valueOf(JOptionPane.showInputDialog("Inserte la provincia del alumno")));
+        a1.setCodpostal(String.valueOf(JOptionPane.showInputDialog("Inserte el codigo postal del alumno")));
+        a1.setAlergias(Integer.parseInt(JOptionPane.showInputDialog("Inserte 0 si el alumno tiene alergias. En caso contrario inserte 1")));
+        a1.setDesAlergias(String.valueOf(JOptionPane.showInputDialog("En caso de presentar alergias, especifiquelo")));
     }
 }
